035f2d9cb1f54abb4ba7a705c6000fe3cbc7f544
Adelina Batereanu <adelinabatereanu@gmail.com>
2025-05-06 13:08:43 +0200

    fetch_byteme.py created parse_offer

--- src/fetch_byteme.py
+++ src/fetch_byteme.py
@@ -8,67 +8,82 @@ load_dotenv()
 
 API_KEY = os.getenv("BYTEME_API_KEY")
 BASE_URL = "https://byteme.gendev7.check24.fun/app/api/products/data"
-params = {
-    "street":      "Meisenstraße",
-    "houseNumber": "7",
-    "city":        "Höhenkirchen-Siegertsbrunn",
-    "plz":         "85635"
-}
 headers = {
     "X-Api-Key": API_KEY
 }
 
-def fetch_offers():
+def fetch_offers(street, house_number, city, plz):
+    """
+    Contact ByteMe API and retrieves offers for the given address
+    Returns: 
+        List[Dict]: A list of raw csv rows as dictionaries
+    """
+    params = {
+    "street":      street,
+    "houseNumber": house_number,
+    "city":        city,
+    "plz":         plz
+}
     response = requests.get(BASE_URL, params=params, headers=headers)
     if response.status_code != 200:
         return []
     text_stream = io.StringIO(response.text)
     reader = csv.DictReader(text_stream)
-    print(response.status_code)
+    # print(response.status_code)
     return list(reader)
 
-def main():
-    offers = fetch_offers()
-    if not offers:
-        print("No offers found")
-        return
-    for offer in offers:
-        name = offer["providerName"]
-        speed = int(offer["speed"])
-        cost_in_cent = int(offer["monthlyCostInCent"])
-        cost = cost_in_cent / 100
-        duration = int(offer["durationInMonths"])
-        after_two_years_cost_in_cent = int(offer["afterTwoYearsMonthlyCost"])
-        after_two_years_cost = after_two_years_cost_in_cent / 100
-        connection_type = offer["connectionType"]
-        installation = offer["installationService"]
-        installation = True if installation == "true" else False
-        tv = offer["tv"]
-        max_age = offer["maxAge"]
-        max_age = int(max_age) if max_age else None
-        voucher_type = offer["voucherType"]
-        voucher_value = offer["voucherValue"]
-        voucher_value = int(voucher_value) if voucher_value else 0
-        if voucher_value == 0:
-            voucher_desc = ""     # no voucher
-        elif voucher_type == "percentage":
-            voucher_desc = f"{voucher_value}% off"
-        else:
-            voucher_desc = f"{voucher_value} ({voucher_type})" # in case voucher_type not "percentage"
-        unlimited = False if offer["limitFrom"] else True
-        # if max_age is not None:
-        #     age_text = f", for people under {max_age}"
-        # else:
-        #     age_text = ""
-        # print(
-        #     f" - {name}: {speed} Mbps for {cost:.2f} for the first {duration} months,", 
-        #     f"{voucher_desc if voucher_desc else ''}", 
-        #     f"{after_two_years_cost:.2f} after,",
-        #     f"{'Unlimited' if unlimited else 'Capped'} data",
-        #     age_text
-        #     )
-       
-   
+def parse_offer(offer):
+    """
+    Convert a raw CSV row dict into a fully typed offer dict. 
+    """
+    name = offer["providerName"]
+    speed = int(offer["speed"])
+    cost_in_cent = int(offer["monthlyCostInCent"])
+    cost = cost_in_cent / 100
+    duration = int(offer["durationInMonths"])
+    post_promo_in_cent = int(offer["afterTwoYearsMonthlyCost"])
+    post_promo_cost = post_promo_in_cent / 100
+    connection_type = offer["connectionType"]
+    installation = offer["installationService"]
+    installation = True if installation == "true" else False
+    tv = offer["tv"]
+    max_age = offer["maxAge"]
+    max_age = int(max_age) if max_age else None
+    voucher_type = offer["voucherType"]
+    voucher_value = offer["voucherValue"]
+    voucher_value = int(voucher_value) if voucher_value else 0
+    if voucher_value == 0:
+        voucher_desc = ""     # no voucher
+    elif voucher_type == "percentage":
+        voucher_desc = f"{voucher_value}% off"
+    else:
+        voucher_desc = f"{voucher_value} ({voucher_type})" # in case voucher_type not "percentage"
+    unlimited = False if offer["limitFrom"] else True
+    return {
+        "name": name,
+        "speed_mbps": speed,
+        "cost_eur": cost,
+        "voucher": voucher_desc,
+        "duration_months": duration,
+        "post_promo_cost_eur": post_promo_cost,
+        "connection_type": connection_type,
+        "installation_included": installation,
+        "tv": tv,
+        "max_age_limit": max_age,
+        "is_unlimited": unlimited
+    }
+
+def main(street, house_number, city, plz):
+    """
+    Fetch offers and create a list of parsed dictionaries
+    Return:
+        Empty list if no offers found
+        List[Dict] of parced offers
+    """
+    raw = fetch_offers(street, house_number, city, plz)
+    if not raw:
+        return []
+    return [parse_offer(r) for r in raw]
 
 if __name__ == "__main__":
-    main()
+    main("Musterstraße", "7", "Berlin", "12345")
