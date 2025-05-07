472faa61db774f19fd67608e145713457bffc46f
Adelina Batereanu <adelinabatereanu@gmail.com>
2025-05-07 13:35:53 +0200

    fetch_verbyndich.py send request to api for page 0

--- src/fetch_verbydich.py
+++ src/fetch_verbydich.py
@@ -3,18 +3,24 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-BASE_URL = "https://verbyndich.gendev7.check24.fun/check24/data/"
+BASE_URL = "https://verbyndich.gendev7.check24.fun/check24/data"
 API_KEY = os.getenv("VERBYNDICH_API_KEY")
 
-address = "Frauenstrasse;11;München;80469"
-params = {
-            "apiKey": API_KEY,
-            "page": 0
-        }
 
-def fetch_verbyndich(address, params):
-    response = requests.post(BASE_URL, params=params, data=address)
+
+# TODO: check for umlaut (encode utf-8)
+
+def fetch_verbyndich():
+    params = {
+            "apiKey": API_KEY,
+            "page": 0,
+        }
+    address = "Frauenstrasse;11;Munchen;80469"
+    response = requests.post(BASE_URL, params=params, data=address, allow_redirects=False, timeout=10)  
     print(response.status_code)
+    print(response.text[:500])
     return
 
-fetch_verbyndich(address, params)
+# data = fetch_verbyndich(params)
+# print(data[0])
+fetch_verbyndich()
